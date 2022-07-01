9d35c6724ebba930a628c85ad6a327baab22b8e7
LucasKalil-Programador <82661706+LucasKalil-Programador@users.noreply.github.com>
2022-07-01 11:27:51 -0300

    adicionado comentaris

--- Cadastro projetos/Program.cs
+++ Cadastro projetos/Program.cs
@@ -8,6 +8,7 @@ namespace Cadastro_projetos
         [STAThread]
         static void Main()
         {
+            // teste
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
