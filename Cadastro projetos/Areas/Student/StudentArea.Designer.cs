c1fa9a4249447c4fffc440729d2b4a5ad8941cfb
LucasKalil-Programador <82661706+LucasKalil-Programador@users.noreply.github.com>
2022-07-02 16:22:10 -0300

    Atualizado Student Area

---
+++ Cadastro projetos/Areas/Student/StudentArea.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace Cadastro_projetos
+{
+    partial class StudentArea
+    {
+        /// <summary> 
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Designer de Componentes
+
+        /// <summary> 
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.RegisterButton = new System.Windows.Forms.Button();
+            this.ViewAndEditButton = new System.Windows.Forms.Button();
+            this.Register = new Cadastro_projetos.Areas.Register();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.Register, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 85.62122F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.37878F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(595, 412);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 5;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 35F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 35F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel2.Controls.Add(this.RegisterButton, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.ViewAndEditButton, 3, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 352);
+            this.tableLayoutPanel2.Margin = new System.Windows.Forms.Padding(0);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(595, 60);
+            this.tableLayoutPanel2.TabIndex = 0;
+            // 
+            // RegisterButton
+            // 
+            this.RegisterButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.RegisterButton.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.RegisterButton.Location = new System.Drawing.Point(62, 3);
+            this.RegisterButton.Name = "RegisterButton";
+            this.RegisterButton.Size = new System.Drawing.Size(202, 54);
+            this.RegisterButton.TabIndex = 0;
+            this.RegisterButton.Text = "Cadastro";
+            this.RegisterButton.UseVisualStyleBackColor = true;
+            // 
+            // ViewAndEditButton
+            // 
+            this.ViewAndEditButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ViewAndEditButton.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.ViewAndEditButton.Location = new System.Drawing.Point(329, 3);
+            this.ViewAndEditButton.Name = "ViewAndEditButton";
+            this.ViewAndEditButton.Size = new System.Drawing.Size(202, 54);
+            this.ViewAndEditButton.TabIndex = 2;
+            this.ViewAndEditButton.Text = "Visualizar / Edição";
+            this.ViewAndEditButton.UseVisualStyleBackColor = true;
+            // 
+            // Register
+            // 
+            this.Register.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Register.Location = new System.Drawing.Point(3, 3);
+            this.Register.Name = "Register";
+            this.Register.Size = new System.Drawing.Size(589, 346);
+            this.Register.TabIndex = 1;
+            // 
+            // StudentArea
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "StudentArea";
+            this.Size = new System.Drawing.Size(595, 412);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private TableLayoutPanel tableLayoutPanel1;
+        private TableLayoutPanel tableLayoutPanel2;
+        private Button RegisterButton;
+        private Button ViewAndEditButton;
+        private Areas.Register Register;
+    }
+}
---
+++ Cadastro projetos/Areas/Student/ViewAndEdit.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cadastro_projetos.Areas.Student
+{
+    public partial class ViewAndEdit : UserControl
+    {
+        public ViewAndEdit()
+        {
+            InitializeComponent();
+        }
+    }
+}
